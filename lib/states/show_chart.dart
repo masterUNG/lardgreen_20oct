23e0b763300616c0520d61661a3bfc64bbe8c0bf
manyou66 <wanpracha.n@rmutsv.ac.th>
2022-04-28 15:59:06 +0700

    28ApprilFinished

--- lib/states/show_chart.dart
+++ lib/states/show_chart.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lardgreen/models/sqllite_model.dart';
 import 'package:lardgreen/utility/my_constant.dart';
 import 'package:lardgreen/utility/sqllite_helper.dart';
+import 'package:lardgreen/widgets/show_progress.dart';
+import 'package:lardgreen/widgets/show_text.dart';
+import 'package:lardgreen/widgets/show_title.dart';
 
 class ShowChart extends StatefulWidget {
   const ShowChart({Key? key}) : super(key: key);
@@ -10,6 +14,10 @@ class ShowChart extends StatefulWidget {
 }
 
 class _ShowChartState extends State<ShowChart> {
+  var sqlModels = <SQLiteModel>[];
+  bool load = true;
+  bool? haveData;
+
   @override
   void initState() {
     super.initState();
@@ -17,9 +25,22 @@ class _ShowChartState extends State<ShowChart> {
   }
 
   Future<void> readMyChart() async {
-    print('readMyChart Work');
+    if (sqlModels.isNotEmpty) {
+      sqlModels.clear();
+    }
+
     await SQLiteHelper().readAllDatabase().then((value) {
-      print('value SQLite ==>> $value');
+      if (value.isEmpty) {
+        haveData = false;
+      } else {
+        haveData = true;
+        for (var item in value) {
+          SQLiteModel model = item;
+          sqlModels.add(model);
+        }
+      }
+      load = false;
+      setState(() {});
     });
   }
 
@@ -28,10 +49,79 @@ class _ShowChartState extends State<ShowChart> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        title: ShowText(
+          lable: 'ตะกร้า',
+          textStyle: MyConstant().h2Style(),
+        ),
         elevation: 0,
         foregroundColor: MyConstant.dark,
         backgroundColor: Colors.white,
       ),
+      body: load
+          ? const ShowProgress()
+          : haveData!
+              ? newContent()
+              : Center(
+                  child: ShowText(
+                    lable: 'ยังไม่มีสินค้าในตะกร้า',
+                    textStyle: MyConstant().h1Style(),
+                  ),
+                ),
     );
   }
+
+  Widget newContent() => Column(
+        children: [
+          Divider(
+            color: MyConstant.dark,
+          ),
+          Row(
+            children: [
+              ShowTitle(title: 'สินค้าจากร้าน :'),
+              ShowText(lable: sqlModels[0].nameSeller),
+            ],
+          ),
+          Divider(
+            color: MyConstant.dark,
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ShowText(
+                  lable: 'สินค้า',
+                  textStyle: MyConstant().h2Style(),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ShowText(
+                  lable: 'ราคา',
+                  textStyle: MyConstant().h2Style(),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ShowText(
+                  lable: 'จำนวน',
+                  textStyle: MyConstant().h2Style(),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ShowText(
+                  lable: 'รวม',
+                  textStyle: MyConstant().h2Style(),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: SizedBox(),
+              ),
+              
+            ],
+          ),
+          Divider(color: MyConstant.dark,),
+        ],
+      );
 }
